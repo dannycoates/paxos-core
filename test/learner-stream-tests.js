8b136b2f96e60f86f9c06a9492dd13f58041aca2
Danny Coates <dannycoates@gmail.com>
2012-12-27 22:40:01 -0800

    started implementing the Learner

---
+++ test/learner-stream-tests.js
@@ -0,0 +1,115 @@
+var assert = require('assert')
+var inherits = require('util').inherits
+var Stream = require('stream')
+var EventEmitter = require('events').EventEmitter
+var LearnerStream = require('../lib/learner-stream')(inherits, Stream)
+var Proposal = require('../lib/proposal')()
+var FakeLearner = EventEmitter
+
+function makeFacts(count, start) {
+	var facts = []
+	var start = start || 1
+	for (var i = start; i < start + count; i++) {
+		facts.push(new Proposal(0, i, 999))
+	}
+	return facts
+}
+
+function randomSort(a, b) {
+	return Math.random() - 0.5
+}
+
+describe('LearnerStream', function () {
+
+	describe('on(data)' , function () {
+
+		var learner = new FakeLearner()
+		var ls = null
+
+		beforeEach(function () {
+			ls = new LearnerStream(learner)
+		})
+
+		afterEach(function () {
+			learner.removeAllListeners('fact')
+			ls.removeAllListeners('data')
+		})
+
+		it('emits data events immediately when learner emits sequentially',
+			function (done) {
+				var count = 10
+				var i = 1
+				var facts = makeFacts(count)
+				ls.on('data', function (fact) {
+					assert.equal(facts.length, count - i)
+					assert.equal(fact.instance, i++)
+					if (i > count) {
+						done()
+					}
+				})
+				while (facts.length) {
+					learner.emit('fact', facts.shift())
+				}
+			}
+		)
+
+		it('emits data in instance order when learner emits out of order',
+			function (done) {
+				var count = 100
+				var i = 1
+				var facts = makeFacts(count).sort(randomSort)
+				ls.on('data', function (fact) {
+					assert.equal(fact.instance, i++)
+					if (i > count) {
+						done()
+					}
+				})
+				while (facts.length) {
+					learner.emit('fact', facts.shift())
+				}
+			}
+		)
+
+		it('emits data in instance order when learner emits in order and out',
+			function (done) {
+				var count = 100
+				var i = 1
+				var facts = makeFacts(20)
+					.concat(makeFacts(10, 21).sort(randomSort))
+					.concat(makeFacts(50, 31))
+					.concat(makeFacts(10, 81).sort(randomSort))
+					.concat(makeFacts(10, 91))
+				ls.on('data', function (fact) {
+					assert.equal(fact.instance, i++)
+					if (i > count) {
+						done()
+					}
+				})
+				while (facts.length) {
+					learner.emit('fact', facts.shift())
+				}
+			}
+		)
+
+		it('ignores facts it has already handled',
+			function (done) {
+				var count = 100
+				var i = 1
+				var facts = makeFacts(10)
+					.concat(makeFacts(10).sort(randomSort))
+					.concat(makeFacts(40, 11).sort(randomSort))
+					.concat(makeFacts(40, 11).sort(randomSort))
+					.concat(makeFacts(50, 51))
+				ls.on('data', function (fact) {
+					assert.equal(fact.instance, i++)
+					if (i > count) {
+						done()
+					}
+				})
+				while (facts.length) {
+					learner.emit('fact', facts.shift())
+				}
+			}
+		)
+	})
+})
---
+++ test/learner-tests.js
@@ -0,0 +1,77 @@
+var assert = require('assert')
+var inherits = require('util').inherits
+var EventEmitter = require('events').EventEmitter
+var Learner = require('../lib/learner')(inherits, EventEmitter)
+var Proposal = require('../lib/proposal')()
+
+describe('Learner', function () {
+
+	describe('decide' , function () {
+
+		var learner = null
+
+		beforeEach(function () {
+			learner = new Learner(2)
+		})
+
+		it('keeps new proposals before there is a majority', function () {
+			var proposal = new Proposal(0, 1, 2)
+			learner.decide(proposal)
+			assert.equal(proposal, learner.instanceProposals[1][0])
+		})
+
+		it('only keeps one proposal for a given acceptor', function () {
+			var proposal = new Proposal(0, 1, 2)
+			learner.decide(proposal)
+			learner.decide(proposal)
+			assert.equal(learner.instanceProposals[1].length, 1)
+		})
+
+		it('emits a fact when a majority is reached', function (done) {
+			var proposal0 = new Proposal(0, 1, 2)
+			var proposal1 = new Proposal(1, 1, 2)
+			learner.on('fact', done.bind(null, null))
+			learner.decide(proposal0)
+			learner.decide(proposal1)
+		})
+
+		it('deletes the instanceProposals when a fact is chosen', function () {
+			var proposal0 = new Proposal(0, 1, 2)
+			var proposal1 = new Proposal(1, 1, 2)
+			learner.decide(proposal0)
+			learner.decide(proposal1)
+			assert.equal(learner.instanceProposals[1], undefined)
+		})
+
+		it('emits the proposal with the largest ballot', function (done) {
+			var proposal0 = new Proposal(0, 1, 2)
+			var proposal1 = new Proposal(1, 1, 3)
+			learner.on('fact', function (fact) {
+				assert.equal(fact, proposal1)
+				done()
+			})
+			learner.decide(proposal0)
+			learner.decide(proposal1)
+		})
+
+		it('the chosen proposal is stored as a fact', function () {
+			var proposal0 = new Proposal(0, 1, 2)
+			var proposal1 = new Proposal(1, 1, 3)
+			learner.decide(proposal0)
+			learner.decide(proposal1)
+			assert.equal(learner.instanceFacts[1], proposal1)
+		})
+
+		it('skips proposals for known facts', function () {
+			var proposal0 = new Proposal(0, 1, 2)
+			var proposal1 = new Proposal(1, 1, 3)
+			learner.decide(proposal0)
+			learner.decide(proposal1)
+
+			var proposal2 = new Proposal(2, 1, 4)
+			learner.decide(proposal2)
+			assert.equal(learner.instanceProposals[1], undefined)
+		})
+
+	})
+})
